6015d3988fd25d542182dbba9c8bb18740cdbf60
Chazaarg <chazarreta.patricio@gmail.com>
2019-07-12 12:18:44 -0300

    fix datetype

--- src/Constraints/DateType.php
+++ src/Constraints/DateType.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace Patlidator\Constraints;
 
 class DateType
@@ -6,30 +6,24 @@ class DateType
     private $options;
     private $message;
     
-    function __construct($options = null)
+    public function __construct($options = null)
     {
-        if($options["message"]){
+        if ($options["message"]) {
             $this->message = $options["message"];
             unset($options["message"]);
         }
          
         $this->options = $options;
     }
-    public function getMessage(){
+    public function getMessage()
+    {
         return $this->message;
     }
-    public function validate($value){
-        $year = date("Y", strtotime($value));
-        $month = date("m", strtotime($value));
-        $day = date("d", strtotime($value));
-        $isValid= checkdate($month, $day, $year);
-
-        if($isValid === false){
-            $this->errors[] = [
-                "campo" => $field,
-                "valor" => $value,
-                "message" => "Campo inválido."
-            ];
+    public function validate($value)
+    {
+        $isValid= strtotime($value);
+        if (!$isValid) {
+            return "Fecha inválida";
         }
     }
 }
